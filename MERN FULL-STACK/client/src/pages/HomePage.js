72290a89b80ca0c187f4424478fd6cc6d255166a
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2022-06-17 01:56:56 -0300

    useState Context

--- MERN FULL-STACK/client/src/pages/HomePage.js
+++ MERN FULL-STACK/client/src/pages/HomePage.js
@@ -3,19 +3,19 @@ import { Link } from "react-router-dom";
 import { PostCard } from "../components/PostCard";
 import { VscEmptyWindow } from "react-icons/vsc";
 
-export function HomePage() {
-  const { posts } = usePosts();
+export function HomePage() { //Esto es para que el contexto sea accesible desde cualquier componente.
+  const { posts } = usePosts(); //Hago un hook para usar el contexto.
 
-  const renderPost = () => {
-    if (posts.length === 0)
+  const renderPost = () => { //Función que renderiza las publicaciones.
+    if (posts.length === 0) //Si no hay publicaciones, muestro un mensaje.
       return (
-        <div className="flex flex-col justify-center items-center">
-          <VscEmptyWindow className="w-48 h-48 text-white" />
-          <h1 className="text-white text-2xl">There are no posts</h1>
+        <div className="flex flex-col justify-center items-center"> 
+          <VscEmptyWindow className="w-48 h-48 text-white" /> 
+          <h1 className="text-white text-2xl">There are no posts</h1> 
         </div>
       );
 
-    return (
+    return ( //Si hay publicaciones, renderizo una card por cada una.
       <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
         {posts.map((post) => (
           <PostCard key={post._id} post={post} />
@@ -24,7 +24,7 @@ export function HomePage() {
     );
   };
 
-  return (
+  return ( //Renderizo el contenido.
     <main>
       <header className="flex justify-between items-center my-4">
         <h1 className="text-2xl text-gray-300 font-bold">
